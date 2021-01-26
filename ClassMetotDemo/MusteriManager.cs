faec0e9530f10857b7a8ccca1e0d544655fa9673
Fatma Betül Uyar <fbluyar2@gmail.com>
2021-01-26 16:58:20 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri eklendi :"+musteri.Name+" "+musteri.Surname);
+        }
+       public void Update(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri güncellendi :" + musteri.Name + " " + musteri.Surname);
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri silindi :" + musteri.Name + " " + musteri.Surname);
+        }
+        public void List(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri listelendi :" + musteri.Name + " " + musteri.Surname);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.ID = 1;
+            musteri1.Name = "Betül";
+            musteri1.Surname = "Uyar";
+            musteri1.City = "Kocaeli";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.List(musteri1);
+            musteriManager.Add(musteri1);
+            musteriManager.Update(musteri1);
+            musteriManager.Delete(musteri1);
+
+        }
+    }
+}
